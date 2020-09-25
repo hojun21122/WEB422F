62998092ee30343441aa142355abe5efb77ac77b
Hojun Hwang <hojun21122@gmail.com>
2020-09-25 18:19:47 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,82 @@
+/*********************************************************************************
+* WEB422 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Ho Jun Hwang Student ID: 112384169 Date: 2020/09/25
+* Heroku Link: _______________________________________________________________
+*
+********************************************************************************/ 
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dataService = require("./modules/data-service.js");
+
+const myData = dataService("mongodb+srv://dbUser:ghwns211@cluster0.vor5b.mongodb.net/sample_supplies?retryWrites=true&w=majority");
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+// ************* API Routes
+
+// POST /api/sales (NOTE: This route must read the contents of the request body)
+app.post("/api/sales", (req, res) => {
+    myData.addNewSale(req.body).then(()=>{
+        console.log("suceess");
+    }).catch((err)=>{
+        console.log("fail");
+      });
+  });
+
+
+// GET /api/sales (NOTE: This route must accept the numeric query parameters "page" and "perPage", ie: /api/sales?page=1&perPage=5 )
+app.get("/api/sales", (req, res) =>{
+    myData.getAllSales(req.query.page, req.query.perPage)
+    .then((val)=>{console.log(val)})
+    .catch((val)=>{throw val})
+})
+
+
+// GET /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
+app.get("/api/sales/:id", (req, res) =>{
+    myData.getSaleById(req.params.id)
+    .then(function(val){console.log(val)})
+    .catch(function(val){throw val})
+})
+
+
+// PUT /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8 as well as read the contents of the request body)
+app.put("/api/sales/:id", (req, res) =>{
+    myData.updateSaleById(req.params.id).then(()=>{
+        console.log(req.params.id);
+    }).catch((err)=>{
+        console.log("fail");
+      });
+})
+
+
+// DELETE /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
+app.delete("/api/sales/:id", (req, res) =>{
+    myData.deleteSaleById(req.params.id).then(()=>{
+        console.log(req.params.id);
+    }).catch((err)=>{
+        console.log("fail");
+      });
+})
+
+// ************* Initialize the Service & Start the Server
+
+myData.initialize().then(()=>{
+    app.listen(HTTP_PORT,()=>{
+        console.log(`server listening on: ${HTTP_PORT}`);
+    });
+}).catch((err)=>{
+    console.log(err);
+});
+
